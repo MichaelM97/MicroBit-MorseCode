bbe466502fcaf46ecede9ec19a63fea16caca3e1
Michael McCormick <michael4.mccormick@live.uwe.ac.uk>
2018-03-07 18:02:03 +0000

    Added displaying of dots/dashes when user presses button A

--- build/bbc-microbit-classic-gcc/yotta_build_info.h
+++ build/bbc-microbit-classic-gcc/yotta_build_info.h
@@ -4,8 +4,11 @@
 #define YOTTA_BUILD_YEAR 2018 // UTC year
 #define YOTTA_BUILD_MONTH 3 // UTC month 1-12
 #define YOTTA_BUILD_DAY 7 // UTC day 1-31
-#define YOTTA_BUILD_HOUR 15 // UTC hour 0-24
-#define YOTTA_BUILD_MINUTE 34 // UTC minute 0-59
-#define YOTTA_BUILD_SECOND 54 // UTC second 0-61
-#define YOTTA_BUILD_UUID dc7c5f0f-9508-457f-a125-b3ffed3306b7 // unique random UUID for each build
+#define YOTTA_BUILD_HOUR 18 // UTC hour 0-24
+#define YOTTA_BUILD_MINUTE 0 // UTC minute 0-59
+#define YOTTA_BUILD_SECOND 28 // UTC second 0-61
+#define YOTTA_BUILD_UUID fb1f166b-e402-4e32-a59b-1b1802c2a3e1 // unique random UUID for each build
+#define YOTTA_BUILD_VCS_ID 4141287bd6d7bdbb8cc9c8330b8230657f91a87c // git or mercurial hash
+#define YOTTA_BUILD_VCS_CLEAN 0 // evaluates true if the version control system was clean, otherwise false
+#define YOTTA_BUILD_VCS_DESCRIPTION 4141287 // git describe or mercurial equivalent
 #endif // ndef __YOTTA_BUILD_INFO_H__
--- source/main.cpp
+++ source/main.cpp
@@ -1,25 +1,89 @@
 /**
  * File: main.cpp
  * Author: Michael McCormick
- * Date: 05-02-2018
- * Desc: Basic game that displays randomly generated input sequence
- * and gets user to repeat the same sequence.
- * Copyright: University of West of England 2017
+ * Date: 07-03-2018
+ * Desc: Basic transmission/recieving protocol for morse code sent across 2
+ * microbits connected via wires. Implements a basic encryption cypher.
+ * Copyright: University of West of England 2018
  */
 #include "MicroBit.h"
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 #include <string>
+#include <map>
 
 MicroBit uBit;
+
+//Global variables
+MicroBitImage dot("0,0,0,0,0\n0,0,0,0,0\n0,0,255,0,0\n0,0,0,0,0\n0,0,0,0,0\n");
+
+MicroBitImage dash("0,0,0,0,0\n0,0,0,0,0\n0,255,255,255,0\n0,0,0,0,0\n0,0,0,0,0\n");
+
+std::map<std::string, char> morseCodeMap = {
+   {".-", 'A'},
+   {"-...", 'B'},
+   {"-.-.", 'C'},
+   {"-..", 'D'},
+   {".", 'E'},
+   {"..-.", 'F'},
+   {"--.", 'G'},
+   {"....", 'H'},
+   {"..", 'I'},
+   {".---", 'J'},
+   {"-.-", 'K'},
+   {".-..", 'L'},
+   {"--", 'M'},
+   {"-.", 'N'},
+   {"---", 'O'},
+   {".--.", 'P'},
+   {"--.-", 'Q'},
+   {".-.", 'R'},
+   {"...", 'S'},
+   {"-", 'T'},
+   {"..-", 'U'},
+   {"...-", 'V'},
+   {".--", 'W'},
+   {"-..-", 'X'},
+   {"-.--", 'Y'},
+   {"--..", 'Z'},
+   {".----", '1'},
+   {"..---", '2'},
+   {"...--", '3'},
+   {"....-", '4'},
+   {"-....", '6'},
+   {".....", '5'},
+   {"--...", '7'},
+   {"---..", '8'},
+   {"----.", '9'},
+   {"-----", '0'}
+};
+
+//On short button press (dot)
+void on_button_A_short(MicroBitEvent) {
+	uBit.display.printAsync(dot);
+  uBit.sleep(500);
+  uBit.display.clear();
+}
+
+//On long button press (dash)
+void on_button_A_long(MicroBitEvent) {
+	uBit.display.printAsync(dash);
+  uBit.sleep(500);
+  uBit.display.clear();
+}
 
 int main()
 {
     //Initialise the micro:bit runtime.
     uBit.init();
 
-    uBit.display.scroll("Hello World!");
+    //Listen for button presses
+		uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, on_button_A_short);
+		uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_LONG_CLICK, on_button_A_long);
+		uBit.sleep(100);
+
+
 
     // If main exits, there may still be other fibers running or
     // registered event handlers etc.
